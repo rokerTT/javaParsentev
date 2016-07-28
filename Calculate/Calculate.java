e9bb591f9f68e46caa86ba44cd9af9060be769a5
Segey Vasilev <bessonnyiserega@yandex.ru>
2016-07-28 14:08:37 +0300

    Summ

--- Calculate/Calculate.java
+++ Calculate/Calculate.java
@@ -13,6 +13,9 @@ public class Calculate {
     public static void main(String[] arg){
         
         System.out.println("Calculate...");
-        System.out.println("Second commit!!!...");
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int summ = first + second;
+        System.out.println("Summ ="+summ);
     }
 }
